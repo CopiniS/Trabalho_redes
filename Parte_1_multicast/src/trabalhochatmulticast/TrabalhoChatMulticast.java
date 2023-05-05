4b120aeab7b68f8b810da97a8d188698b1de3db1
Copini - PC <Copini - PC@DESKTOP-JM8QMBE>
2023-05-05 00:09:47 -0300

    comit tcp

---
+++ Parte_1_multicast/src/trabalhochatmulticast/TrabalhoChatMulticast.java
@@ -0,0 +1,74 @@
+package trabalhochatmulticast;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+import javax.swing.JOptionPane;
+
+public class TrabalhoChatMulticast {
+
+    public static void main(String[] args) {
+
+        InetAddress srvIP = null;
+        int srvPort = 0;
+        MulticastSocket mtcSock = null;
+        
+        if(args.length != 2){
+            JOptionPane.showMessageDialog(null, "Argumentos inválidos", "ERRO", JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+        else{
+        
+            try {
+                srvIP = InetAddress.getByName(args[0]);
+                srvPort = Integer.parseInt(args[1]);
+                mtcSock = new MulticastSocket(srvPort);
+
+                mtcSock.joinGroup(srvIP); 
+
+            } catch (UnknownHostException ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage(), "ERRO", JOptionPane.ERROR_MESSAGE);
+                System.exit(0);
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage(), "ERRO", JOptionPane.ERROR_MESSAGE);
+                System.exit(0);
+            } 
+
+        JanelaP j = new JanelaP(srvIP, mtcSock, srvPort);
+        j.setVisible(true);
+        j.setLocationRelativeTo(null);
+    }
+    
+//       InetAddress srvIP = null;
+//       int srvPort = 0;
+//        MulticastSocket mtcSock = null;
+//
+//        if(args.length != 2){
+//            JOptionPane.showMessageDialog(null, "Argumentos inválidos", "ERRO", JOptionPane.ERROR_MESSAGE);
+//            System.exit(0);
+//        }
+//        else{
+//            try {
+//                srvIP = InetAddress.getByName("224.0.0.100");
+//              srvPort = Integer.parseInt(args[1]); 
+//                mtcSock = new MulticastSocket(srvPort);
+//
+//                mtcSock.joinGroup(srvIP); 
+//
+//            } catch (UnknownHostException ex) {
+//                JOptionPane.showMessageDialog(null, ex.getMessage(), "ERRO", JOptionPane.ERROR_MESSAGE);
+//                System.exit(0);
+//            } catch (IOException ex) {
+//                JOptionPane.showMessageDialog(null, ex.getMessage(), "ERRO", JOptionPane.ERROR_MESSAGE);
+//                System.exit(0);
+//            } 
+//        }
+//
+//        JanelaP j = new JanelaP(srvIP, mtcSock, srvPort);
+//        j.setVisible(true);
+//        j.setLocationRelativeTo(null);
+//    }
+//
+    }
+}
